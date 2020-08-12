95a7c463449c071f4507c3c54ce0e9220d079f57
blank <vongk7@gmail.com>
2020-08-12 16:49:43 -0500

    Updates

---
+++ precache-manifest.8414b473635ac8f52b90b2a9ca7948da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c9989345ed65f04bdda6f4341c8790",
+    "url": "/react/pokedex/index.html"
+  },
+  {
+    "revision": "761800aaffbba9906122",
+    "url": "/react/pokedex/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "82a0cb7d0f8f8c39a920",
+    "url": "/react/pokedex/static/css/main.bebb91a6.chunk.css"
+  },
+  {
+    "revision": "761800aaffbba9906122",
+    "url": "/react/pokedex/static/js/2.2e9c6de0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/pokedex/static/js/2.2e9c6de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a0cb7d0f8f8c39a920",
+    "url": "/react/pokedex/static/js/main.ee8c7688.chunk.js"
+  },
+  {
+    "revision": "657d27e63380a47ec810",
+    "url": "/react/pokedex/static/js/runtime-main.fa8231ac.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react/pokedex/precache-manifest.8414b473635ac8f52b90b2a9ca7948da.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/pokedex/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
